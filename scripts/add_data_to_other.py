197867e8951def088306d1bc90328ecdb6b320b0
CZL_MAC <13754793902@163.com>
2025-04-27 09:41:03 +0800

    feat：将raw合并到其他文件

--- scripts/add_data_to_other.py
+++ scripts/add_data_to_other.py
@@ -6,20 +6,20 @@ with open("../data/llm_with_data/两轮微调0426的结果_with_data.json", "r")
 
 # 读取 JSON Lines 文件并修改
 new_json = []
-with open("../data/raw_llm/0微调qwen结果.jsonl", "r") as f_in:
-    for i, line in enumerate(f_in):
+with open("../data/llm_with_data_code/qwen_7b没微调的结果_with_data_code.json", "r") as f_in:
+    json_data_1 = json.load(f_in)
+    for i, obj in enumerate(json_data_1):
         # 解析 JSONL 行
-        jsonl_obj = json.loads(line.strip())
 
         # 获取对应的 raw_data（确保 JSON 文件和 JSONL 文件行数相同）
         if i < len(json_data):
             raw_data = json_data[i].get("raw_data", "")
-            jsonl_obj["raw_data"] = raw_data  # 添加 raw_data 字段
-        new_json.append(jsonl_obj)
+            obj["raw_data"] = raw_data  # 添加 raw_data 字段
+        new_json.append(obj)
 
         # 写入新文件
 
-output_path = '../data/llm_with_data/0微调qwen结果_with_data.json'
+output_path = '../data/llm_with_data_code/qwen_7b没微调的结果_with_data_code.json'
 with open(output_path, 'w', encoding='utf-8') as f:
     json.dump(new_json, f, indent=2, ensure_ascii=False)
 
