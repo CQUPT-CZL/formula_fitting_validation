85fdebdb8af51db58154b29e0bf946f348b66a41
CQUPT-CZL <1522518cui@gmail.com>
2025-05-11 09:18:55 +0800

    refactor：json和jsonl读取修改

--- scripts/add_data_to_other.py
+++ scripts/add_data_to_other.py
@@ -1,16 +1,16 @@
 import json
 
 # 读取 JSON 文件
-with open("../data/llm_with_data/两轮微调0426的结果_with_data.json", "r") as f:
+with open("../data/llm_with_data/v2/1.5b_all_sft_with_data.json", "r", encoding='utf-8') as f:
     json_data = json.load(f)  # 解析为列表，每个元素是 {"raw_data": "..."}
 
 # 读取 JSON Lines 文件并修改
 new_json = []
-with open("../data/llm_with_data_code/qwen_7b没微调的结果_with_data_code.json", "r") as f_in:
-    json_data_1 = json.load(f_in)
-    for i, obj in enumerate(json_data_1):
+with open("../data/raw_llm/v2/1.5b_all_sft.jsonl", "r", encoding='utf-8') as f_in:
+    # json_data_1 = json.load(f_in)
+    for i, obj in enumerate(f_in):
         # 解析 JSONL 行
-
+        obj = json.loads(obj)
         # 获取对应的 raw_data（确保 JSON 文件和 JSONL 文件行数相同）
         if i < len(json_data):
             raw_data = json_data[i].get("raw_data", "")
@@ -19,9 +19,8 @@ with open("../data/llm_with_data_code/qwen_7b没微调的结果_with_data_code.j
 
         # 写入新文件
 
-output_path = '../data/llm_with_data_code/qwen_7b没微调的结果_with_data_code.json'
+output_path = '../data/llm_with_data/v2/1.5b_all_sft_with_data.json'
 with open(output_path, 'w', encoding='utf-8') as f:
     json.dump(new_json, f, indent=2, ensure_ascii=False)
-
 
 print("处理完成！结果已保存")
