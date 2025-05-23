5ca35072d12eacae8f85b57dd2c13968115d15aa
CZL_MAC <13754793902@163.com>
2025-05-23 18:23:14 +0800

    fix：修复添加gt_codebug
    怪不得之前所有的都是第一个

--- scripts/add_data_to_other.py
+++ scripts/add_data_to_other.py
@@ -1,12 +1,12 @@
 import json
 
-# # 读取 JSON 文件
+# 读取 JSON 文件
 # with open("../data/llm_with_data/v3/1.5b_no_sft_with_data.json", "r", encoding='utf-8') as f:
 #     json_data = json.load(f)  # 解析为列表，每个元素是 {"raw_data": "..."}
 #
 # # 读取 JSON Lines 文件并修改
 # new_json = []
-# with open("../data/raw_llm/v3/7b_no_sft.jsonl", "r", encoding='utf-8') as f_in:
+# with open("../data/raw_llm/v3/qwen_72b_api_pred.jsonl", "r", encoding='utf-8') as f_in:
 #     # json_data_1 = json.load(f_in)
 #     for i, obj in enumerate(f_in):
 #         # 解析 JSONL 行
@@ -19,7 +19,7 @@ import json
 #
 #         # 写入新文件
 #
-# output_path = '../data/llm_with_data/v3/7b_no_sft_with_data.json'
+# output_path = '../data/llm_with_data/v3/qwen_72b_api_pred_with_data.json'
 # with open(output_path, 'w', encoding='utf-8') as f:
 #     json.dump(new_json, f, indent=2, ensure_ascii=False)
 #
@@ -29,11 +29,11 @@ import json
 
 # 将gt_code也添加到各个文件中
 # 读取 JSON 文件
-with open("../data/llm_with_data_code/v3/1.5b_no_sft_with_data_code.json", "r", encoding='utf-8') as f:
+with open("../data/llm_with_data_code/v3/1.5b_all_sft_with_data_code.json", "r", encoding='utf-8') as f:
     json_data = json.load(f)  # 解析为列表，每个元素是 {"raw_data": "..."}
 
 
-file = "../data/llm_with_data_code/v3/7b_no_sft_with_data_code.json"
+file = "../data/llm_with_data_code/v3/qwen_72b_api_pred_with_data_code.json"
 
 # 读取 JSON Lines 文件并修改
 new_json = []
@@ -52,6 +52,7 @@ with open(file, "r", encoding='utf-8') as f_in:
     for item in json.load(f_in):
         item['gt_code'] = json_data[i]['gt_code']
         new_json.append(item)
+        i += 1
 
 output_path = file
 with open(output_path, 'w', encoding='utf-8') as f:
